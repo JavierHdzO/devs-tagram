942d8029bd546b416b6b0a5d5ad178807509583e
JavierHdzO <fco_hernandez1705@hotmail.com>
2023-02-06 22:41:42 -0600

    020623 F [FIX]: modify vite server config for locating resources

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,18 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import os from 'os';
 
 export default defineConfig({
+    server: {
+        // host: process.env.LARAVEL_SAIL ? Object.values(os.networkInterfaces()).flat().find(info => info?.internal === false)?.address : undefined,
+        hmr: {
+            host: 'localhost',
+        },
+    },
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
+        
         }),
     ],
 });
